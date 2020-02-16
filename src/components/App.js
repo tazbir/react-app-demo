319954ba8abce71fcd93b0d894dc3b930097ec7b
tazbirs <tazbirs@gmail.com>
2020-02-16 20:24:01 +0600

    add functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -3,61 +3,71 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
-import {without} from 'lodash'
+import { without } from 'lodash'
 
 class App extends Component {
-  
-  constructor(){
+
+  constructor() {
     super();
-    this.state={
-      myAppointments:[],
-      lastIndex:0,
-      formDisplay:false
+    this.state = {
+      myAppointments: [],
+      lastIndex: 0,
+      formDisplay: false
     }
-    this.deleteAppointment= this.deleteAppointment.bind(this);
-    this.toggleForm= this.toggleForm.bind(this);
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
   }
 
-  deleteAppointment(apt){
-    let tempApts= this.state.myAppointments;
-    tempApts=without(tempApts, apt);
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    })
+  }
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
 
     this.setState({
-      myAppointments:tempApts
+      myAppointments: tempApts
     });
   }
 
-  toggleForm(){
+  toggleForm() {
     this.setState({
-      formDisplay:!this.state.formDisplay
+      formDisplay: !this.state.formDisplay
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('./data.json')
-      .then(response=> response.json())
-      .then(result=>{
-        const apts= result.map(item=>{
-          item.aptId=this.state.lastIndex;
-          this.setState({lastIndex:this.state.lastIndex+1})
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 })
           return item;
         });
         this.setState({
-          myAppointments:apts
+          myAppointments: apts
         });
       });
   }
 
-  render(){
+  render() {
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm}/>
-                <SearchAppointments/>
-                <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment}/>
+                <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} addAppointment={this.addAppointment} />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment} />
               </div>
             </div>
           </div>
